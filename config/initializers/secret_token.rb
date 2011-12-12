e174549a31a5299f957eb9482c1a86b11fd0f336
mikeebert <mike@mikeebert.net>
2011-12-12 16:38:19 -0600

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Virtucoach::Application.config.secret_token = '8cf777707cfebc179ef22c43859424e5ca52b56098b8e7d7359f27fa012b8c5bf83309fb5d6544729ebf639b580e1578eeb97c7f94b2062233a9a6f9aa4f07a9'
---
+++ db/migrate/20111212223426_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address
+      t.string :intersection
+      t.string :city
+      t.string :state
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
